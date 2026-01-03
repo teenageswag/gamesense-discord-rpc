0b80f9f3fda4b4ecd64f22c9538ae2ce45b872b4
Artem <mailforallaccs@gmail.com>
2026-01-04 04:07:24 +0900

    feat: add initial RPC implementation

--- src/main.go
+++ src/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"os"
+	"os/signal"
 	"rpc/log"
 	"rpc/process"
+	"rpc/rpc"
+	"time"
+
+	"github.com/hugolgst/rich-go/client"
 )
 
 /*
@@ -12,15 +18,55 @@ import (
 	4. Закрываем консоль и продолжаем работу в фоне
 */
 
+var gs *rpc.RPCInfo = rpc.Initialize(
+	"1457019638331347008",
+	"",
+	"Get Good - Get Gamesense",
+	"gs_logo640",
+	"gamesense.pub",
+	"",
+	"",
+	"Get Gamesense",
+	"https://gamesense.pub",
+)
+
 func main() {
-	// 1. Проверка запуска дс
-	log.Info("Проверяем запущен ли дискорд...")
+
+	log.Info("ПРоверяем запущен ли discord")
+
 	if !process.DiscordRunning() {
-		log.Error("Процесс не найден. Пробуем запустить вручную.")
+		log.Info("Процесс не найден. Пробуем запустить вручную")
 		if !process.LaunchDiscord() {
 			return
 		}
-	} else {
-		log.Info("Discord уже запущен")
+		log.Success("Процесс найден.")
+		log.Info("Запускаем RPC")
 	}
+
+	err := client.Login(gs.AppId)
+	if err != nil {
+		log.Error("Не удалось запустить RPC")
+		return
+	}
+	defer client.Logout()
+
+	startTime := time.Now()
+	rpc.UpdatePresence(gs, startTime)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+
+	ticker := time.NewTicker(15 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			rpc.UpdatePresence(gs, startTime)
+		case <-sigChan:
+			log.Info("Завершение работы RPC")
+			return
+		}
+	}
+
 }
--- src/rpc/rpc.go
+++ src/rpc/rpc.go
@@ -1,7 +1,73 @@
 package rpc
 
+import (
+	"rpc/log"
+	"time"
+
+	"github.com/hugolgst/rich-go/client"
+)
+
 /*
-	1. Структура RPC
-	2. Функция инициализации RPC
+	1. Структура RPC -> Готова
+	2. Функция инициализации RPC -> Готова
 	3. Функция обновления RPC
 */
+
+type RPCInfo struct {
+	AppId      string
+	State      string
+	Details    string
+	LargeImage string
+	LargeText  string
+	SmallImage string
+	SmallText  string
+	ButtonName string
+	ButtonUrl  string
+}
+
+func Initialize(id string, state string, details string, largeImage string, largeText string, smallImage string, smallText string, buttonName string, buttonUrl string) *RPCInfo {
+	if id == "" || id == "YOUR_APP_ID" {
+		log.Error("Неверный ID приложения")
+		return nil
+	}
+	return &RPCInfo{
+		AppId:      id,
+		State:      state,
+		Details:    details,
+		LargeImage: largeImage,
+		LargeText:  largeText,
+		SmallImage: smallImage,
+		SmallText:  smallText,
+		ButtonName: buttonName,
+		ButtonUrl:  buttonUrl,
+	}
+}
+
+func UpdatePresence(rpcInfo *RPCInfo, startTime time.Time) {
+	activity := client.Activity{
+		State:      rpcInfo.State,
+		Details:    rpcInfo.Details,
+		LargeImage: rpcInfo.LargeImage,
+		LargeText:  rpcInfo.LargeText,
+		// SmallImage: "rogue",
+		// SmallText:  "Rogue - Level 100",
+
+		Timestamps: &client.Timestamps{
+			Start: &startTime,
+		},
+
+		Buttons: []*client.Button{
+			{
+				Label: rpcInfo.ButtonName,
+				Url:   rpcInfo.ButtonUrl,
+			},
+		},
+	}
+
+	err := client.SetActivity(activity)
+	if err != nil {
+		// log.Error("Ошибка обновления активности")
+	} else {
+		// log.Info("Активность обновлена")
+	}
+}
